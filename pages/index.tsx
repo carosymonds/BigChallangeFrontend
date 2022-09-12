8680207dbc7f7d7e0b0d055b53b8882f3553c7d7
Carolyn Symonds <carolynsymonds@192.168.1.6>
2022-09-12 10:48:23 -0300

    Dialog Modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { Menu, Transition } from '@headlessui/react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { BeakerIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
+import { Fragment, useState } from 'react'
 
 const links = [
   { href: '/account-settings', label: 'Account settings' },
@@ -11,6 +12,9 @@ const links = [
   { href: '/sign-out', label: 'Sign out' },
 ]
 const Home: NextPage = () => {
+
+  let [isOpen, setIsOpen] = useState(true)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -114,9 +118,79 @@ const Home: NextPage = () => {
             </Menu.Items>
           </Transition>
         </Menu>
-        <div>
+        <div className='mt-4'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione exercitationem iste quia numquam itaque laudantium, autem molestias veniam similique rerum fugit alias a expedita inventore quos facilis dolor quas cumque?
         </div>
+        <div className='mt-4'>
+          <h2 className='font-bold text-xl pt-2'>
+            Dialog (Dropdown)
+          </h2>
+          <div className="mt-4">
+          <button onClick={() => setIsOpen(true)} className="flex items-center bg-blue-500 text-white rounded-md px-4 py-2">
+            Open Model
+          </button>
+          <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+              </Transition.Child>
+
+              <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-800"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                  >
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                      <button className="absolute top-3 right-3 text-gray-500">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                      </button>
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-900"
+                      >
+                        Payment successful
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Your payment has been successfully submitted. We’ve sent
+                          you an email with all of the details of your order.
+                        </p>
+                      </div>
+
+                      <div className="mt-4">
+                        <button
+                          type="button"
+                          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          onClick={() => setIsOpen(false)}
+                        >
+                          Got it, thanks!
+                        </button>
+                      </div>
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </div>
+              </div>
+            </Dialog>
+          </Transition>
+          </div>
+        </div>
+
       </div>
     </div>
   )
